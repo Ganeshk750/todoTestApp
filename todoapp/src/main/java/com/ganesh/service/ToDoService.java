97c74278070305e7c461f79030fcebe350dd0ce5
Ganeshk750 <ganesh9ot@gmail.com>
2021-11-23 17:25:11 +0530

    # entity repo service with impl

---
+++ todoapp/src/main/java/com/ganesh/service/ToDoService.java
@@ -0,0 +1,15 @@
+package com.ganesh.service;
+
+import java.util.Optional;
+
+import com.ganesh.model.ToDo;
+
+
+public interface ToDoService {
+	
+	public ToDo newTodo(ToDo todo);
+	public java.util.List<ToDo> listOfToDo();
+	public Optional<ToDo> getOneToDo(Integer todoId);
+	public Optional<ToDo> updateToDo(Integer todoId, ToDo todo);
+
+}
---
+++ todoapp/src/main/java/com/ganesh/service/ToDoServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ganesh.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ganesh.model.ToDo;
+import com.ganesh.repository.ToDoRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.var;
+
+@Service
+@RequiredArgsConstructor
+public class ToDoServiceImpl implements ToDoService {
+	
+	private final ToDoRepository repository;
+
+	@Override
+	public ToDo newTodo(ToDo todo) {
+		return this.repository.save(todo);
+	}
+
+	@Override
+	public List<ToDo> listOfToDo() {
+		return this.repository.findAll();
+	}
+
+	@Override
+	public Optional<ToDo> getOneToDo(Integer todoId) {
+	    var todo = this.repository.findById(todoId);
+		return todo;
+	}
+
+	@Override
+	public Optional<ToDo> updateToDo(Integer todoId, ToDo todo) {
+		return this.repository.findById(todoId).map(oldTodo -> this.repository.save(todo));
+	}
+
+}
