5fde9fc398f6b87291e687c0d991026d8a08cea6
Ganeshk750 <ganesh9ot@gmail.com>
2021-11-24 11:25:00 +0530

    deleted

--- todoapp/src/main/java/com/ganesh/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ganesh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ganesh.model.ToDo;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
-
-}
