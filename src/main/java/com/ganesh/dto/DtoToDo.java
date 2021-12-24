133c82ef90a48d58c6316a4b4416aadf3d442d3f
Ganeshk750 <ganesh9ot@gmail.com>
2021-12-24 12:48:32 +0530

    # swagger integration done

--- src/main/java/com/ganesh/dto/DtoToDo.java
+++ src/main/java/com/ganesh/dto/DtoToDo.java
@@ -1,16 +1,22 @@
 package com.ganesh.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Api(value="Todo Model Information")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DtoToDo {
 	
+	@ApiModelProperty(value="ToDo Id")
 	private Integer id;
+	@ApiModelProperty(value="ToDo Title")
 	private String title;
+	@ApiModelProperty(value="ToDo Status")
 	private boolean done;
 
 }
